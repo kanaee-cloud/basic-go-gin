633aefeb4843702db36a63aef8dd15d59a06646c
aaltansa <arsalnaufal2007@gmail.com>
2024-11-14 08:04:12 +0700

    first commit

---
+++ app/service/publisher.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"base-gin/app/repository"
+	"base-gin/app/domain/dto"
+)
+
+type PublisherService struct {
+	repo *repository.PublisherRepository
+}
+
+// Corrected function signature with matching parameter names
+func newPublisherService(publisherRepo *repository.PublisherRepository) *PublisherService {
+	return &PublisherService{repo: publisherRepo}
+}
+
+func (s *PublisherService) Create(params *dto.PublisherCreateReq)(*dto.PublisherCreateResp, error){
+	newItem := params.ToEntity()
+
+	err := s.repo.Create(&newItem)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp dto.PublisherCreateResp
+	resp.FromEntity(&newItem)
+
+	return &resp, nil
+}
+
